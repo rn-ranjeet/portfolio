575b95409e17c5da6d81504785c7375bc9b038e0
rn-ranjeet <mauryaranjeet@gmail.com>
2021-03-12 09:48:18 +0530

    Updated

--- assets/JS/style.js
+++ assets/JS/style.js
@@ -1,5 +1,11 @@
 
+// function myFunction() {
+//    var element = document.getElementsByClassName("navbar-nav");
+//    element[0].classList.toggle("nv");
+// }
+
 function myFunction() {
-   var element = document.getElementsByClassName("navbar-nav");
-   element[0].classList.toggle("nv");
+
+	var element = document.getElementsByClassName("nav-links");
+	element[0].classList.toggle("nv-phone");
 }
